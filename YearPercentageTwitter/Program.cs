bfb39ab467d313b1028be44e1183d4c554896142
edwardiiibarrier@gmail.com <edwardiiibarrier@gmail.com>
2021-11-08 22:08:17 -0600

    Created a class to hide API sensitive data, a method to deal with finding the percentage of the year, and added the code to tweet to twitter once the application is running.

---
+++ YearPercentageTwitter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Tweetinvi;
+using System.Threading.Tasks;
+using ApiKeys;
+
+namespace YearPercentageTwitter
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            
+
+            TwitterClient UserClient = new TwitterClient(ApiKeysLibrary.ApiKey, ApiKeysLibrary.ApiSecretKey, ApiKeysLibrary.AccessToken, ApiKeysLibrary.AccessSecretToken);
+            var user = await UserClient.Users.GetAuthenticatedUserAsync();
+            string newTweet = Convert.ToString($"We are {YearPercentage()}% through the year!");
+            var tweet = await UserClient.Tweets.PublishTweetAsync(newTweet);
+
+            Console.WriteLine("Tweet Successful");
+
+        }
+
+        //Get the percentage of the year
+        private static double YearPercentage()
+        {
+            double dayOfYear = DateTime.Now.DayOfYear;
+            double percentageOfYear = dayOfYear / 365 * 100;
+            percentageOfYear = Math.Round(percentageOfYear, 0);
+            return percentageOfYear;
+        }
+
+
+    }
+}
